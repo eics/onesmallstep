833b77da9c7514b150ad48a576616d1d0e5c2050
moriahlim <moriahlim@yahoo.com>
2019-12-03 23:18:13 -0500

    Try to implement upload

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 from helpers import apology, login_required, lookup, usd
 
@@ -43,9 +44,45 @@ def index():
 @app.route("/goal", methods=["GET", "POST"])
 def goal():
     if request.method == "POST":
-        return apology("TODO")
+        return apology("Goal TODO")
     else:
         return render_template("goal.html")
+
+
+# Upload file
+# From https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
+UPLOAD_FOLDER = 'csvfiles/'
+ALLOWED_EXTENSIONS = {'csv'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename).replace("\\","/"))
+            print("2")
+            return apology("Uploaded")
+        else:
+            flash('File not allowed')
+            return redirect(request.url)
+    else:
+        return render_template("upload.html")
 
 
 def errorhandler(e):
