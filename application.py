59d21531ab8c440b100a06b70b134ceb8309e0aa
Terry Ni <terry@Terrys-MacBook-Pro-8.local>
2019-12-05 22:25:51 -0500

    no more deprecated sessions! proper google login :-)

--- application.py
+++ application.py
@@ -1,16 +1,31 @@
-import os
 import csv
+import json
+import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
-from flask_session import Session
+#from flask_session import Session
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
 from quickstart import createtask
+from user import User
 from helpers import apology, login_required, lookup, usd
+
+
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Configure application
 app = Flask(__name__)
@@ -26,15 +41,34 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "303120742144-oa0smbgd3o6799hgmaa9lhl3q921ga5q.apps.googleusercontent.com")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "dkIpcyh4qvqkHB_mruW-g6Nn")
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-app.secret_key = 'many random bytes'
-
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 db = SQL("sqlite:///goals.db")
+
+app.secret_key = 'manyrandombytes'
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -42,13 +76,93 @@ def index():
     if request.method == "POST":
         searchterm = request.form.get("searchterm")
         return redirect("/searchresults/%s" % (searchterm))
-    else:
+    else: 
         return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = requests.get(GOOGLE_DISCOVERY_URL).json()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = requests.get(GOOGLE_DISCOVERY_URL).json()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # hit the URL from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+    # Create a user in your db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect("/")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
 # searchresult.html loops through the results, makes button for each one that sends out the id for that result
-def searchresult(searchterm):
 @app.route("/searchresults/<searchterm>", methods=["GET", "POST"]) 
+def searchresult(searchterm):
     results = db.execute("SELECT * FROM goals WHERE name LIKE '%{}%'".format(searchterm))
     return render_template("searchresult.html", results=results, term=searchterm)
 
@@ -74,7 +188,6 @@ def goal(goal_id):
         if not request.form.get("frequency"):
             return apology("Commit yourself to a frequency!!", 403)
         frequency = request.form.get("frequency")
-        flash('Adding tasks; please remain on this page until complete.')
         createtask(startdate, frequency, result, steps) 
         flash('Added to Google Tasks (view in Calendar/Gmail/App)!')
     return render_template("goal.html", name=name, goaldata=result, steps=steps, goal_id=goal_id)
