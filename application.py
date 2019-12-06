d31e7d31a00f301ca1d96cbb18d887117644ab34
Terry Ni <terry@Terrys-MacBook-Pro-8.local>
2019-12-05 23:39:17 -0500

    fixed search

--- application.py
+++ application.py
@@ -159,7 +159,11 @@ def logout():
 # searchresult.html loops through the results, makes button for each one that sends out the id for that result
 @app.route("/searchresults/<searchterm>", methods=["GET", "POST"]) 
 def searchresult(searchterm):
-    results = db.execute("SELECT * FROM goals WHERE name LIKE '%{}% OR desc LIKE '%{searchterm}%' AND (private = 0 OR user_id = {id})".format(searchterm=searchterm, id=current_user.get_id()))
+    if current_user is None:
+        results = db.execute("SELECT * FROM goals WHERE name LIKE '%{}% OR desc LIKE '%{searchterm}%' AND private = 0".format(searchterm=searchterm)
+    else:
+        results = db.execute("SELECT * FROM goals WHERE name LIKE '%{}% OR desc LIKE '%{searchterm}%' AND (private = 0 OR user_id = {id})".format(searchterm=searchterm, id=current_user.get_id()))
+    
     return render_template("searchresult.html", results=results, term=searchterm)
 
 
